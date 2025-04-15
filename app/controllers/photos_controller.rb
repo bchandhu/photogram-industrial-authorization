f29b87efa0ae932129b546bf204f149f2b8837bf
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2025-04-15 03:30:29 +0000

    Add ownership check for editing and deleting photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,6 +2,12 @@ class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
   before_action :ensure_current_user_is_owner, only: [:destroy, :update, :edit]
 
+  def ensure_current_user_is_owner
+    if current_user != @photo.owner
+      redirect_back(fallback_location: root_url, alert: "You can only edit or delete your own photos.")
+    end
+  end
+
   # GET /photos or /photos.json
   def index
     @photos = Photo.all
